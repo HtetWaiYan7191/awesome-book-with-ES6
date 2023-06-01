12c8c861b931a7b55c39457daebb3a4ea97c28f4
HtetWaiYan7191 <htetwaiyan7191@gmail.com>
2023-06-01 15:32:40 +0630

    Create different js files in modules folder

---
+++ modules/bookManager.js
@@ -0,0 +1,28 @@
+/* eslint-disable class-methods-use-this */
+import Books from './book.js';
+
+export default class BookManager {
+  constructor() {
+    this.books = this.getFromLocalStorage();
+  }
+
+  addBook(title, author) {
+    const book = new Books(title, author);
+    this.books.push(book);
+    this.saveToLocalStorage();
+  }
+
+  removeBook(index) {
+    this.books.splice(index, 1);
+    this.saveToLocalStorage();
+  }
+
+  saveToLocalStorage() {
+    localStorage.setItem('Books', JSON.stringify(this.books));
+  }
+
+  getFromLocalStorage() {
+    const storeBook = JSON.parse(localStorage.getItem('Books')) || [];
+    return storeBook;
+  }
+}
